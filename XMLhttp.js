28899a263733e3d6d0309fd3eb5c4be0c28ab824
THARUN KUMAR V A <124011988+Tharunkumarva@users.noreply.github.com>
2023-08-23 22:43:48 +0530

    Add files via upload

---
+++ XMLhttp.js
@@ -0,0 +1,105 @@
+//Flag
+// let XMLHttpRequest = require('xhr2');
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+// xhr.onload = function () {
+//         let data = JSON.parse(xhr.responseText);
+        
+//         for (let country of data) {
+//             let countryName = country.flags.png;
+//             console.log(countryName);
+//         }
+    
+// }
+
+// xhr.send();
+
+
+// // country names
+// let XMLHttpRequest = require('xhr2');
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+// xhr.onload = function () {
+//         let data = JSON.parse(xhr.responseText);
+        
+//         for (let country of data) {
+//             let countryName = country.name.common;
+//             console.log(countryName);
+//         }
+    
+// }
+
+// xhr.send();
+
+
+// region
+// let XMLHttpRequest = require('xhr2');
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+// xhr.onload = function () {
+//         let data = JSON.parse(xhr.responseText);
+        
+//         for (let country of data) {
+//             let countryName = country.region;
+//             console.log(countryName);
+//         }
+    
+// }
+
+// xhr.send();
+
+// // sun-region
+
+// let XMLHttpRequest = require('xhr2');
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+// xhr.onload = function () {
+//         let data = JSON.parse(xhr.responseText);
+        
+//         for (let country of data) {
+//             let countryName = country.subregion;
+//             console.log(countryName);
+//         }
+    
+// }
+
+// xhr.send();
+
+
+// population
+
+// let XMLHttpRequest = require('xhr2');
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://restcountries.com/v3.1/all');
+
+// xhr.onload = function () {
+//         let data = JSON.parse(xhr.responseText);
+        
+//         for (let country of data) {
+//             let countryName = country.population;
+//             console.log(countryName);
+//         }
+    
+// }
+
+// xhr.send();
+
---
+++ json.js
@@ -0,0 +1,14 @@
+let obj1 = {
+name:"persoan 1",
+age:5
+
+}
+let obj2 = {
+    age:5,
+    name:"persoan 1"
+}
+
+// console.log(obj1 === obj2);
+// console.log(obj1,2 ===obj2,1);
+// console.log(obj1 == obj2);
+// console.log(JSON.stringify(obj1)===JSON.stringify(obj2));
